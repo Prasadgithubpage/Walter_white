79673300d588e5336e8df1ee97ffac860767708f
Prasadgithubpage <115395228+Prasadgithubpage@users.noreply.github.com>
2024-05-24 17:11:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,6 @@
 import logging
 import logging.config
-
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("imdbpy").setLevel(logging.ERROR)
-
-from pyrogram import Client, __version__
+from pyrogram import Client, __version__, filters
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
@@ -20,8 +13,13 @@ from plugins import web_server
 
 PORT = "8080"
 
-class Bot(Client):
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
+class Bot(Client):
     def __init__(self):
         super().__init__(
             name=SESSION,
@@ -32,6 +30,7 @@ class Bot(Client):
             plugins={"root": "plugins"},
             sleep_threshold=5,
         )
+        self.files = []  # To store the list of files
 
     async def start(self):
         b_users, b_chats = await db.get_banned()
@@ -44,7 +43,7 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
-        app = web.AppRunner(await web_server())
+        app = web.AppRunner(await self.create_web_server())
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
@@ -54,36 +53,13 @@ class Bot(Client):
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped. Bye.")
-    
+
     async def iter_messages(
         self,
         chat_id: Union[int, str],
         limit: int,
         offset: int = 0,
     ) -> Optional[AsyncGenerator["types.Message", None]]:
-        """Iterate through a chat sequentially.
-        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
-        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
-        single call.
-        Parameters:
-            chat_id (``int`` | ``str``):
-                Unique identifier (int) or username (str) of the target chat.
-                For your personal cloud (Saved Messages) you can simply use "me" or "self".
-                For a contact that exists in your Telegram address book you can use his phone number (str).
-                
-            limit (``int``):
-                Identifier of the last message to be returned.
-                
-            offset (``int``, *optional*):
-                Identifier of the first message to be returned.
-                Defaults to 0.
-        Returns:
-            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
-        Example:
-            .. code-block:: python
-                for message in app.iter_messages("pyrogram", 1, 15000):
-                    print(message.text)
-        """
         current = offset
         while True:
             new_diff = min(200, limit - current)
@@ -94,7 +70,51 @@ class Bot(Client):
                 yield message
                 current += 1
 
+    async def search_files(self, query: str) -> list:
+        # Dummy search function, replace with actual search logic
+        # For now, just return a list of dummy file names based on the query
+        files = [f"{query}_file_{i}.txt" for i in range(1, 11)]
+        self.files = files
+        return files[:10]
+
+    async def get_file_by_index(self, index: int) -> str:
+        # Dummy function to retrieve a file by its index
+        if 0 <= index < len(self.files):
+            return self.files[index]
+        return "File not found"
+
+    async def handle_search(self, request):
+        params = await request.json()
+        query = params.get('query', '')
+        files = await self.search_files(query)
+        return web.json_response({"files": files})
+
+    async def handle_get_file(self, request):
+        params = await request.json()
+        index = int(params.get('index', -1))
+        file = await self.get_file_by_index(index)
+        return web.json_response({"file": file})
+
+    async def create_web_server(self):
+        app = web.Application()
+        app.router.add_post('/search_files', self.handle_search)
+        app.router.add_post('/get_file', self.handle_get_file)
+        return app
+
+    async def on_message(self, message: types.Message):
+        query = message.text
+        files = await self.search_files(query)
+        if files:
+            response = "Found the following files:\n" + "\n".join([f"{i+1}. {file}" for i, file in enumerate(files)])
+        else:
+            response = "No matching files found."
+        await message.reply_text(response)
 
 app = Bot()
-app.run()
 
+# Add handler to capture all text messages and perform search
+@app.on_message(filters.text & ~filters.command)
+async def handle_text_message(client, message):
+    await client.on_message(message)
+
+app.run()
